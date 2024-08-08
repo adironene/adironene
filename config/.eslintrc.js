10ae1edbc9028e828c8d49a7a7e9623157312344
Phoebe Tang <49921653+adironene@users.noreply.github.com>
2024-08-09 00:09:29 +0200

    upgraded ESLint + changed eslint config

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,23 +1,20 @@
-/** @type {import('eslint').FlatConfig} */
-const config = [
-  {
-    ignores: ['node_modules/**'],
-  },
-  {
-    files: ['src/**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
+module.exports = {
+  ignorePatterns: ['node_modules/**'],
+  overrides: [
+    {
+      files: ['src/**/*.js'],
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      rules: {
+        'no-unused-vars': 'warn',
+        'no-console': 'off',
+        'eqeqeq': 'error',
+        'indent': ['error', 2],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+      },
     },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off',
-      'eqeqeq': 'error',
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
-    },
-  },
-];
-
-module.exports = config;
+  ],
+};
